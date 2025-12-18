e7618815a92cf29ca7b7cd755fb237411dcfe438
Muskan2771 <155989406+Muskan2771@users.noreply.github.com>
2025-12-18 18:31:16 +0530

    Update Q9.py

--- Core-Python/Assignments/Assignment_8/Q9.py
+++ Core-Python/Assignments/Assignment_8/Q9.py
@@ -1,12 +1,14 @@
-def palindrome(num):
-    temp = num
+def is_palindrome(num):
+    original = num
     rev = 0
     while num > 0:
-        rev = rev * 10 + num % 10
+        digit = num % 10
+        rev = rev * 10 + digit
         num //= 10
-    if temp == rev:
-        print("Palindrome")
-    else:
-        print("Not Palindrome")
+    return original == rev
 
-palindrome(121)
+num = int(input("Enter the number: "))
+if is_palindrome(num):
+    print(f"{num} is a palindrome.")
+else:
+    print(f"{num} is not a palindrome.")
