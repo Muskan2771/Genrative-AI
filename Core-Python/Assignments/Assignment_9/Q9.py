8ec040a0996c46f4533cff880549783db8b8d012
Muskan2771 <155989406+Muskan2771@users.noreply.github.com>
2025-12-18 20:06:48 +0530

    Update Q9.py
    Updated power function to accept user input for base and exponent.

--- Core-Python/Assignments/Assignment_9/Q9.py
+++ Core-Python/Assignments/Assignment_9/Q9.py
@@ -1,6 +1,9 @@
 def power(m, n):
     if n == 0:
         return 1
-    return m * power(m, n-1)
+    return m * power(m, n - 1)
 
-print(power(2, 3))
+m = int(input("Enter base: "))
+n = int(input("Enter power: "))
+
+print("Result:", power(m, n))
