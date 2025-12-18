63abd428832e072c4b129965379a09653d643208
Muskan2771 <155989406+Muskan2771@users.noreply.github.com>
2025-12-18 19:16:39 +0530

    Update Q1.py

--- Core-Python/Assignments/Assignment_9/Q1.py
+++ Core-Python/Assignments/Assignment_9/Q1.py
@@ -1,11 +1,15 @@
-def fact(n):
+def fact(num):
+    if num == 1:
+        return 1
+    else:
+        return num * fact(num - 1)
+        
+def sum_fact(n):
     if n == 1:
         return 1
-    return n * fact(n-1)
+    else:
+        return fact(n) + sum_fact(n - 1)
 
-def fact_sum(n):
-    if n == 1:
-        return fact(1)
-    return fact(n) + fact_sum(n-1)
-
-print(fact_sum(4))
+num = int(input("Enter the number: "))
+result = sum_fact(num)
+print("Sum of factorials:", result)
