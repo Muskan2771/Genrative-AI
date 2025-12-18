467a23f05820690bdf4471dbfdd47014fe3e5187
Muskan2771 <155989406+Muskan2771@users.noreply.github.com>
2025-12-18 17:43:49 +0530

    Update Q4.py
    Refactor sum_odd function to sum_series and return result instead of printing. Added user input for n value.

--- Core-Python/Assignments/Assignment_8/Q4.py
+++ Core-Python/Assignments/Assignment_8/Q4.py
@@ -1,8 +1,10 @@
-def sum_odd(n):
-    s = 0
-    for i in range(1, n+1):
-        if i % 2 != 0:
-            s += i
-    print("Sum of odd numbers:", s)
+def sum_series(n):
+    sum_s=0
+    for i in range(1,n+1):
+            if i%2!=0:
+              sum_s+=i
+    return sum_s
 
-sum_odd(10)
+n=int(input("Enter n value: "))
+result=sum_series(n)   
+print("Sum of series is: ",result) 
