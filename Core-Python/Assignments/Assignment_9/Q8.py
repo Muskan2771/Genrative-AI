a3b550f770fb9035391af0256571f164c8dc4568
Muskan2771 <155989406+Muskan2771@users.noreply.github.com>
2025-12-18 20:03:34 +0530

    Update Q8.py

--- Core-Python/Assignments/Assignment_9/Q8.py
+++ Core-Python/Assignments/Assignment_9/Q8.py
@@ -1,10 +1,15 @@
-def prime(num, i=2):
-    if num <= 2:
-        return True
+def is_prime(num, i=2):
+    if num <= 1:
+        return False
     if i == num:
         return True
     if num % i == 0:
         return False
-    return prime(num, i+1)
+    return is_prime(num, i + 1)
 
-print(prime(7))
+num = int(input("Enter the number: "))
+
+if is_prime(num):
+    print(num, "is a Prime number")
+else:
+    print(num, "is Not a Prime number")
