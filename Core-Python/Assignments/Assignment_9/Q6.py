27f9ef557e6a23fdcf1222a8fab7041ec6ca9180
Muskan2771 <155989406+Muskan2771@users.noreply.github.com>
2025-12-18 19:42:01 +0530

    Update Q6.py

--- Core-Python/Assignments/Assignment_9/Q6.py
+++ Core-Python/Assignments/Assignment_9/Q6.py
@@ -1,8 +1,10 @@
 def fib(n):
-    if n == 1 or n == 2:
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
-    return fib(n-1) + fib(n-2)
+    return fib(n - 1) + fib(n - 2)
 
-n = 6
-for i in range(1, n+1):
+n = int(input("Enter number of terms: "))
+for i in range(n):
     print(fib(i), end=" ")
